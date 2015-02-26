adc99d0e58b68670cb46d7c39f4df2f29d67da93
rmagee88 <rmagee88@gmail.com>
2015-02-25 19:59:49 -0800

    helper functions and cron script

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var httprequest = require('http-request');
+
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -30,14 +32,70 @@ exports.readListOfUrls = function(callback){
   callback(varray);
 };
 
-exports.isUrlInList = function(){
+exports.isUrlInList = function(url){
+  exports.readListOfUrls(function(varray){
+    for (var i = 0; i < varray.length; i++){
+      if (varray[i] === url){return true;}
+    }
+  });
+  return false;
 };
 
-exports.addUrlToList = function(){
+exports.addUrlToList = function(url){
+  fs.appendFile(exports.paths.list, url, {encoding: 'utf8'}, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('The ' + url + ' was appended to file!');
+  });
 };
 
-exports.isURLArchived = function(){
+exports.isURLArchived = function(url, callback){
+  fs.readFile(exports.paths.archivedSites + /* backslash? */url, {encoding: 'utf8'}, function(error, data){
+    if (error){
+      console.log(error);
+      exports.addUrlToList(url);
+      callback(url, false, data);
+    }else{
+      callback(url, true, data);
+    }
+  });
 };
 
-exports.downloadUrls = function(){
+
+var addFile = function(urlObj, html){
+  fs.writeFile(exports.paths.archivedSites + '/' + urlObj.url, {encoding: 'utf8'}, html, function(error){
+    console.log(error);
+  });
+  exports.removeFromList(urlObj.url);
 };
+
+exports.downloadUrls = function(urlArray){
+  for (var i = 0; i < urlArray.length; i++){
+    httprequest.get(urlArray[i], function(err, response){
+      if (err) {
+        console.log(err);
+      }else{
+        addFile(this, response.buffer.toString());
+      }
+    }.bind({url: urlArray[i]}));
+  }
+};
+
+exports.removeFromList = function(url){
+  exports.readListOfUrls(function(varray){
+    var newArray = _.filter(varray, function(item){
+      if (item !== url){return true;}
+    });
+    fs.writeFile(exports.paths.list, {encoding: 'utf8'}, newArray.join('\n'), function(err){
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
+};
+
+
+
+
+
